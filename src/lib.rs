222ed1969af990cafd3798f685da32ac065505f9
LeonineKing1199 <christian.mazakas@gmail.com>
2020-06-19 17:04:09 -0700

    Add tests for maximum alignment checking

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,12 @@ fn next_aligned(num_bytes: usize, alignment: usize) -> usize {
     }
 }
 
+struct Header<T> {
+    data_: *mut T,
+    len_: usize,
+    cap_: usize,
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -28,34 +34,49 @@ mod tests {
         assert_eq!(next_aligned(16, 32), 32);
         assert_eq!(next_aligned(16, 512), 512);
     }
-}
 
-struct Header<T> {
-    data_: *mut T,
-    len_: usize,
-    cap_: usize,
-}
+    #[repr(align(512))]
+    struct OverAligned {
+        data: [u8; 512],
+    }
 
-fn get_layout<T>(cap: usize) -> Layout {
-    let header_size = mem::size_of::<Header<T>>();
-    let num_bytes = if cap == 0 {
-        header_size
-    } else {
-        next_aligned(header_size, mem::align_of::<T>()) + cap * mem::size_of::<T>()
-    };
+    #[test]
+    fn max_align_test() {
+        let header_alignment = mem::align_of::<Header<()>>();
 
-    Layout::from_size_align(num_bytes, max_align::<T>()).unwrap()
-}
+        assert!(mem::align_of::<i32>() <= mem::align_of::<Header<()>>());
+        assert_eq!(max_align::<i32>(), header_alignment);
 
-struct MiniVec<T> {
-    buf_: *mut u8,
-    phantom_: PhantomData<T>,
+        assert!(mem::align_of::<u8>() <= mem::align_of::<Header<()>>());
+        assert_eq!(max_align::<u8>(), header_alignment);
+
+        assert!(mem::align_of::<OverAligned>() > mem::align_of::<Header<()>>());
+        assert_eq!(max_align::<OverAligned>(), mem::align_of::<OverAligned>());
+    }
 }
 
 fn max_align<T>() -> usize {
     let align_t = mem::align_of::<T>();
     let header_align = mem::align_of::<Header<T>>();
     std::cmp::max(align_t, header_align)
+}
+
+fn make_layout<T>(cap: usize) -> Layout {
+    let alignment = max_align::<T>();
+
+    let header_size = mem::size_of::<Header<T>>();
+    let num_bytes = if cap == 0 {
+        header_size
+    } else {
+        next_aligned(header_size, alignment) + cap * mem::size_of::<T>()
+    };
+
+    Layout::from_size_align(num_bytes, alignment).unwrap()
+}
+
+struct MiniVec<T> {
+    buf_: *mut u8,
+    phantom_: PhantomData<T>,
 }
 
 impl<T> MiniVec<T> {

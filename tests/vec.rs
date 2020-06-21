e05652f6aae58d384b76b31517289951da755d3f
LeonineKing1199 <christian.mazakas@gmail.com>
2020-06-21 13:34:08 -0700

    Fix indentation

--- tests/vec.rs
+++ tests/vec.rs
@@ -4,69 +4,69 @@ use minivec::MiniVec;
 
 #[test]
 fn size_test() {
-  assert_eq!(
-    std::mem::size_of::<MiniVec<i32>>(),
-    std::mem::size_of::<*const i32>()
-  );
+    assert_eq!(
+        std::mem::size_of::<MiniVec<i32>>(),
+        std::mem::size_of::<*const i32>()
+    );
 }
 
 #[test]
 fn minivec_default_constructed() {
-  let v: MiniVec<i32> = MiniVec::new();
-  assert_eq!(v.capacity(), 0);
-  assert_eq!(v.len(), 0);
-  assert!(v.is_empty());
+    let v: MiniVec<i32> = MiniVec::new();
+    assert_eq!(v.capacity(), 0);
+    assert_eq!(v.len(), 0);
+    assert!(v.is_empty());
 
-  let v: MiniVec<i32> = Default::default();
-  assert_eq!(v.capacity(), 0);
-  assert_eq!(v.len(), 0);
-  assert!(v.is_empty());
+    let v: MiniVec<i32> = Default::default();
+    assert_eq!(v.capacity(), 0);
+    assert_eq!(v.len(), 0);
+    assert!(v.is_empty());
 }
 
 #[test]
 fn minivec_push() {
-  let mut v: MiniVec<i32> = MiniVec::new();
+    let mut v: MiniVec<i32> = MiniVec::new();
 
-  assert_eq!(v.len(), v.capacity());
+    assert_eq!(v.len(), v.capacity());
 
-  v.push(1);
-  v.push(2);
-  v.push(3);
+    v.push(1);
+    v.push(2);
+    v.push(3);
 
-  assert_eq!(v.len(), 3);
-  assert!(v.capacity() >= v.len());
+    assert_eq!(v.len(), 3);
+    assert!(v.capacity() >= v.len());
 
-  let mut v: MiniVec<String> = MiniVec::new();
+    let mut v: MiniVec<String> = MiniVec::new();
 
-  assert_eq!(v.len(), v.capacity());
+    assert_eq!(v.len(), v.capacity());
 
-  v.push(String::from("Hello"));
-  v.push(String::from("Rust"));
-  v.push(String::from("World!"));
+    v.push(String::from("Hello"));
+    v.push(String::from("Rust"));
+    v.push(String::from("World!"));
 
-  assert_eq!(v.len(), 3);
-  assert!(v.capacity() >= v.len());
+    assert_eq!(v.len(), 3);
+    assert!(v.capacity() >= v.len());
 
-  let mut v: MiniVec<String> = MiniVec::new();
+    let mut v: MiniVec<String> = MiniVec::new();
 
-  assert_eq!(v.len(), v.capacity());
+    assert_eq!(v.len(), v.capacity());
 
-  for _ in 0..32 {
-    v.push(String::from("Hello, world!"));
-  }
+    for _ in 0..32 {
+        v.push(String::from("Hello, world!"));
+    }
 
-  assert_eq!(v.len(), 32);
-  assert!(v.capacity() >= v.len());
+    assert_eq!(v.len(), 32);
+    assert!(v.capacity() >= v.len());
 }
 
 #[test]
 fn deref_test() {
-  let mut v: MiniVec<i32> = MiniVec::new();
-  v.push(1);
-  v.push(2);
-  v.push(3);
+    let mut v: MiniVec<i32> = MiniVec::new();
+    v.push(1);
+    v.push(2);
+    v.push(3);
 
-  assert_eq!(v[0], 1);
-  assert_eq!(v[1], 2);
-  assert_eq!(v[2], 3);
+    assert_eq!(v[0], 1);
+    assert_eq!(v[1], 2);
+    assert_eq!(v[2], 3);
 }

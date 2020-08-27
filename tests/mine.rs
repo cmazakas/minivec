4576eca0571515629ee8316343a560e57db40364
LeonineKing1199 <christian.mazakas@gmail.com>
2020-08-26 20:36:51 -0700

    Fix indentation

--- tests/mine.rs
+++ tests/mine.rs
@@ -3,17 +3,19 @@ extern crate minivec;
 use minivec::mini_vec;
 use minivec::MiniVec;
 
+use std::convert::From;
+
 #[test]
 fn minivec_default_constructed() {
-  let v: MiniVec<i32> = MiniVec::new();
-  assert_eq!(v.capacity(), 0);
-  assert_eq!(v.len(), 0);
-  assert!(v.is_empty());
+    let v: MiniVec<i32> = MiniVec::new();
+    assert_eq!(v.capacity(), 0);
+    assert_eq!(v.len(), 0);
+    assert!(v.is_empty());
 
-  let v: MiniVec<i32> = Default::default();
-  assert_eq!(v.capacity(), 0);
-  assert_eq!(v.len(), 0);
-  assert!(v.is_empty());
+    let v: MiniVec<i32> = Default::default();
+    assert_eq!(v.capacity(), 0);
+    assert_eq!(v.len(), 0);
+    assert!(v.is_empty());
 }
 
 #[test]
@@ -30,338 +32,338 @@ fn minivec_as_mut1() {
 
 #[test]
 fn minivec_push() {
-  let mut v: MiniVec<i32> = MiniVec::new();
+    let mut v: MiniVec<i32> = MiniVec::new();
 
-  assert_eq!(v.len(), v.capacity());
+    assert_eq!(v.len(), v.capacity());
 
-  v.push(1);
-  v.push(2);
-  v.push(3);
+    v.push(1);
+    v.push(2);
+    v.push(3);
 
-  assert_eq!(v.len(), 3);
-  assert!(v.capacity() >= v.len());
+    assert_eq!(v.len(), 3);
+    assert!(v.capacity() >= v.len());
 
-  let mut v: MiniVec<String> = MiniVec::new();
+    let mut v: MiniVec<String> = MiniVec::new();
 
-  assert_eq!(v.len(), v.capacity());
+    assert_eq!(v.len(), v.capacity());
 
-  v.push(String::from("Hello"));
-  v.push(String::from("Rust"));
-  v.push(String::from("World!"));
+    v.push(String::from("Hello"));
+    v.push(String::from("Rust"));
+    v.push(String::from("World!"));
 
-  assert_eq!(v.len(), 3);
-  assert!(v.capacity() >= v.len());
+    assert_eq!(v.len(), 3);
+    assert!(v.capacity() >= v.len());
 
-  let mut v: MiniVec<String> = MiniVec::new();
+    let mut v: MiniVec<String> = MiniVec::new();
 
-  assert_eq!(v.len(), v.capacity());
+    assert_eq!(v.len(), v.capacity());
 
-  for _ in 0..32 {
-    v.push(String::from("Hello, world!"));
-  }
+    for _ in 0..32 {
+        v.push(String::from("Hello, world!"));
+    }
 
-  assert_eq!(v.len(), 32);
-  assert!(v.capacity() >= v.len());
+    assert_eq!(v.len(), 32);
+    assert!(v.capacity() >= v.len());
 }
 
 #[test]
 fn minivec_deref_test() {
-  let mut v: MiniVec<i32> = MiniVec::new();
-  v.push(1);
-  v.push(2);
-  v.push(3);
+    let mut v: MiniVec<i32> = MiniVec::new();
+    v.push(1);
+    v.push(2);
+    v.push(3);
 
-  assert_eq!(v[0], 1);
-  assert_eq!(v[1], 2);
-  assert_eq!(v[2], 3);
+    assert_eq!(v[0], 1);
+    assert_eq!(v[1], 2);
+    assert_eq!(v[2], 3);
 }
 
 #[test]
 fn minivec_dedup_by_test() {
-  let mut v = mini_vec![1, 2, 1, 1, 3, 3, 3, 4, 5, 4];
-  v.dedup_by(|x, y| x == y);
+    let mut v = mini_vec![1, 2, 1, 1, 3, 3, 3, 4, 5, 4];
+    v.dedup_by(|x, y| x == y);
 
-  assert_eq!(v, [1, 2, 1, 3, 4, 5, 4]);
+    assert_eq!(v, [1, 2, 1, 3, 4, 5, 4]);
 }
 
 #[test]
 fn minivec_dedup_needs_drop() {
-  let mut v: MiniVec<Box<_>> = mini_vec![Box::new(1), Box::new(1), Box::new(2), Box::new(3)];
-  v.dedup();
+    let mut v: MiniVec<Box<_>> = mini_vec![Box::new(1), Box::new(1), Box::new(2), Box::new(3)];
+    v.dedup();
 
-  assert_eq!(v.len(), 3);
+    assert_eq!(v.len(), 3);
 }
 
 #[test]
 fn minivec_with_capacity() {
-  let size = 128;
-  let mut v: MiniVec<i32> = MiniVec::with_capacity(size);
+    let size = 128;
+    let mut v: MiniVec<i32> = MiniVec::with_capacity(size);
 
-  assert_eq!(v.len(), 0);
-  assert_eq!(v.capacity(), size);
+    assert_eq!(v.len(), 0);
+    assert_eq!(v.capacity(), size);
 
-  v.push(1);
-  v.push(2);
-  v.push(3);
+    v.push(1);
+    v.push(2);
+    v.push(3);
 
-  assert_eq!(v.len(), 3);
-  assert_eq!(v.capacity(), size);
+    assert_eq!(v.len(), 3);
+    assert_eq!(v.capacity(), size);
 }
 
 #[test]
 fn minivec_extend_from_slice() {
-  let a = [2, 3, 4, 5];
-  let mut v = mini_vec![1];
-  v.extend_from_slice(&a);
+    let a = [2, 3, 4, 5];
+    let mut v = mini_vec![1];
+    v.extend_from_slice(&a);
 
-  assert_eq!(v.len(), 5);
+    assert_eq!(v.len(), 5);
 
-  v.extend_from_slice(&[6, 7, 8]);
-  assert_eq!(v.len(), 8);
+    v.extend_from_slice(&[6, 7, 8]);
+    assert_eq!(v.len(), 8);
 
-  assert_eq!(a.len(), 4);
-  assert_eq!(a, [2, 3, 4, 5]);
+    assert_eq!(a.len(), 4);
+    assert_eq!(a, [2, 3, 4, 5]);
 
-  let a: MiniVec<_> = [2, 3, 4, 5].iter().map(|x| x.to_string()).collect();
-  let mut v = mini_vec![String::from("1")];
-  v.extend_from_slice(&a);
+    let a: MiniVec<_> = [2, 3, 4, 5].iter().map(|x| x.to_string()).collect();
+    let mut v = mini_vec![String::from("1")];
+    v.extend_from_slice(&a);
 
-  assert_eq!(v.len(), 5);
+    assert_eq!(v.len(), 5);
 
-  v.extend_from_slice(&[6.to_string(), 7.to_string(), 8.to_string()]);
-  assert_eq!(v.len(), 8);
+    v.extend_from_slice(&[6.to_string(), 7.to_string(), 8.to_string()]);
+    assert_eq!(v.len(), 8);
 
-  assert_eq!(a.len(), 4);
-  assert_eq!(
-    a,
-    [2.to_string(), 3.to_string(), 4.to_string(), 5.to_string()]
-  );
+    assert_eq!(a.len(), 4);
+    assert_eq!(
+        a,
+        [2.to_string(), 3.to_string(), 4.to_string(), 5.to_string()]
+    );
 }
 
 #[test]
 fn minivec_from_raw_part() {
-  use std::{mem, ptr};
+    use std::{mem, ptr};
 
-  let v = mini_vec![1, 2, 3];
-  let mut v = mem::ManuallyDrop::new(v);
+    let v = mini_vec![1, 2, 3];
+    let mut v = mem::ManuallyDrop::new(v);
 
-  let p = v.as_mut_ptr();
-  let len = v.len();
-  let cap = v.capacity();
+    let p = v.as_mut_ptr();
+    let len = v.len();
+    let cap = v.capacity();
 
-  unsafe {
-    for i in 0..len as isize {
-      ptr::write(p.offset(i), 4 + i);
+    unsafe {
+        for i in 0..len as isize {
+            ptr::write(p.offset(i), 4 + i);
+        }
+
+        let rebuilt = MiniVec::from_raw_part(p);
+        assert_eq!(rebuilt, [4, 5, 6]);
+        assert_eq!(rebuilt.capacity(), cap);
+        assert_eq!(rebuilt.len(), len);
     }
-
-    let rebuilt = MiniVec::from_raw_part(p);
-    assert_eq!(rebuilt, [4, 5, 6]);
-    assert_eq!(rebuilt.capacity(), cap);
-    assert_eq!(rebuilt.len(), len);
-  }
 }
 
 #[test]
 fn minivec_from_raw_parts() {
-  use std::{mem, ptr};
+    use std::{mem, ptr};
 
-  let v = mini_vec![1, 2, 3];
-  let mut v = mem::ManuallyDrop::new(v);
+    let v = mini_vec![1, 2, 3];
+    let mut v = mem::ManuallyDrop::new(v);
 
-  let p = v.as_mut_ptr();
-  let len = v.len();
-  let cap = v.capacity();
+    let p = v.as_mut_ptr();
+    let len = v.len();
+    let cap = v.capacity();
 
-  unsafe {
-    for i in 0..len as isize {
-      ptr::write(p.offset(i), 4 + i);
+    unsafe {
+        for i in 0..len as isize {
+            ptr::write(p.offset(i), 4 + i);
+        }
+
+        let rebuilt = MiniVec::from_raw_parts(p, len, cap);
+        assert_eq!(rebuilt, [4, 5, 6]);
+        assert_eq!(rebuilt.capacity(), cap);
+        assert_eq!(rebuilt.len(), len);
     }
-
-    let rebuilt = MiniVec::from_raw_parts(p, len, cap);
-    assert_eq!(rebuilt, [4, 5, 6]);
-    assert_eq!(rebuilt.capacity(), cap);
-    assert_eq!(rebuilt.len(), len);
-  }
 }
 
 #[test]
 fn minivec_insert() {
-  let mut vec = mini_vec![1, 2, 3];
+    let mut vec = mini_vec![1, 2, 3];
 
-  vec.insert(1, 4);
-  assert_eq!(vec, [1, 4, 2, 3]);
+    vec.insert(1, 4);
+    assert_eq!(vec, [1, 4, 2, 3]);
 
-  vec.insert(4, 5);
-  assert_eq!(vec, [1, 4, 2, 3, 5]);
+    vec.insert(4, 5);
+    assert_eq!(vec, [1, 4, 2, 3, 5]);
 
-  let mut vec: MiniVec<String> = mini_vec![1.to_string(), 2.to_string(), 3.to_string()];
+    let mut vec: MiniVec<String> = mini_vec![1.to_string(), 2.to_string(), 3.to_string()];
 
-  vec.insert(1, 4.to_string());
-  assert_eq!(
-    vec,
-    [1.to_string(), 4.to_string(), 2.to_string(), 3.to_string()]
-  );
+    vec.insert(1, 4.to_string());
+    assert_eq!(
+        vec,
+        [1.to_string(), 4.to_string(), 2.to_string(), 3.to_string()]
+    );
 
-  vec.insert(4, 5.to_string());
-  assert_eq!(
-    vec,
-    [
-      1.to_string(),
-      4.to_string(),
-      2.to_string(),
-      3.to_string(),
-      5.to_string()
-    ]
-  );
+    vec.insert(4, 5.to_string());
+    assert_eq!(
+        vec,
+        [
+            1.to_string(),
+            4.to_string(),
+            2.to_string(),
+            3.to_string(),
+            5.to_string()
+        ]
+    );
 }
 
 #[test]
 fn minivec_leak() {
-  let x = mini_vec![1, 2, 3];
-  let static_ref: &'static mut [usize] = MiniVec::leak(x);
-  static_ref[0] += 1;
-  assert_eq!(static_ref, &[2, 2, 3]);
+    let x = mini_vec![1, 2, 3];
+    let static_ref: &'static mut [usize] = MiniVec::leak(x);
+    static_ref[0] += 1;
+    assert_eq!(static_ref, &[2, 2, 3]);
 }
 
 #[test]
 fn minivec_pop() {
-  let mut vec = mini_vec![1, 2, 3];
-  assert_eq!(vec.pop(), Some(3));
-  assert_eq!(vec, [1, 2]);
+    let mut vec = mini_vec![1, 2, 3];
+    assert_eq!(vec.pop(), Some(3));
+    assert_eq!(vec, [1, 2]);
 
-  let mut vec = mini_vec![1.to_string(), 2.to_string(), 3.to_string()];
-  assert_eq!(vec.pop(), Some(String::from("3")));
-  assert_eq!(vec, [String::from("1"), String::from("2")]);
+    let mut vec = mini_vec![1.to_string(), 2.to_string(), 3.to_string()];
+    assert_eq!(vec.pop(), Some(String::from("3")));
+    assert_eq!(vec, [String::from("1"), String::from("2")]);
 }
 
 #[test]
 fn minivec_remove() {
-  let mut v = mini_vec![1, 2, 3];
-  assert_eq!(v.remove(1), 2);
-  assert_eq!(v, [1, 3]);
+    let mut v = mini_vec![1, 2, 3];
+    assert_eq!(v.remove(1), 2);
+    assert_eq!(v, [1, 3]);
 }
 
 #[test]
 #[should_panic]
 fn minivec_remove_panic() {
-  let mut v = mini_vec![1, 2, 3];
-  v.remove(v.len());
+    let mut v = mini_vec![1, 2, 3];
+    v.remove(v.len());
 }
 
 #[test]
 fn minivec_remove_item() {
-  let mut vec = mini_vec![1, 2, 3, 1];
+    let mut vec = mini_vec![1, 2, 3, 1];
 
-  vec.remove_item(&1);
+    vec.remove_item(&1);
 
-  assert_eq!(vec, mini_vec![2, 3, 1]);
+    assert_eq!(vec, mini_vec![2, 3, 1]);
 }
 
 #[test]
 fn minivec_test_resize() {
-  let mut vec = mini_vec!["hello"];
-  vec.resize(3, "world");
-  assert_eq!(vec, ["hello", "world", "world"]);
+    let mut vec = mini_vec!["hello"];
+    vec.resize(3, "world");
+    assert_eq!(vec, ["hello", "world", "world"]);
 
-  let mut vec = mini_vec![1, 2, 3, 4];
-  vec.resize(2, 0);
-  assert_eq!(vec, [1, 2]);
+    let mut vec = mini_vec![1, 2, 3, 4];
+    vec.resize(2, 0);
+    assert_eq!(vec, [1, 2]);
 
-  let mut vec = mini_vec![1, 2, 3, 4];
-  vec.resize(vec.len(), -1);
-  assert_eq!(vec, [1, 2, 3, 4]);
+    let mut vec = mini_vec![1, 2, 3, 4];
+    vec.resize(vec.len(), -1);
+    assert_eq!(vec, [1, 2, 3, 4]);
 }
 
 #[test]
 fn minivec_resize_with() {
-  let mut vec = mini_vec![1, 2, 3];
-  vec.resize_with(5, Default::default);
-  assert_eq!(vec, [1, 2, 3, 0, 0]);
+    let mut vec = mini_vec![1, 2, 3];
+    vec.resize_with(5, Default::default);
+    assert_eq!(vec, [1, 2, 3, 0, 0]);
 
-  let mut vec = mini_vec![];
-  let mut p = 1;
-  vec.resize_with(4, || {
-    p *= 2;
-    p
-  });
+    let mut vec = mini_vec![];
+    let mut p = 1;
+    vec.resize_with(4, || {
+        p *= 2;
+        p
+    });
 
-  assert_eq!(vec, [2, 4, 8, 16]);
+    assert_eq!(vec, [2, 4, 8, 16]);
 }
 
 #[test]
 fn minivec_retain() {
-  let mut vec = mini_vec![1, 2, 3, 4];
-  vec.retain(|&x| x % 2 == 0);
-  assert_eq!(vec, [2, 4]);
+    let mut vec = mini_vec![1, 2, 3, 4];
+    vec.retain(|&x| x % 2 == 0);
+    assert_eq!(vec, [2, 4]);
 
-  let mut vec = mini_vec![1, 2, 3, 4, 5];
-  let keep = [false, true, true, false, true];
+    let mut vec = mini_vec![1, 2, 3, 4, 5];
+    let keep = [false, true, true, false, true];
 
-  let mut i = 0;
-  vec.retain(|_| {
-    let should_keep = keep[i];
-    i += 1;
-    should_keep
-  });
+    let mut i = 0;
+    vec.retain(|_| {
+        let should_keep = keep[i];
+        i += 1;
+        should_keep
+    });
 
-  assert_eq!(vec, [2, 3, 5]);
+    assert_eq!(vec, [2, 3, 5]);
 }
 
 #[test]
 fn minivec_shrink_to() {
-  let mut vec = MiniVec::<i32>::with_capacity(10);
-  vec.push(1);
-  vec.push(2);
-  vec.push(3);
+    let mut vec = MiniVec::<i32>::with_capacity(10);
+    vec.push(1);
+    vec.push(2);
+    vec.push(3);
 
-  assert_eq!(vec.capacity(), 10);
-  assert_eq!(vec.len(), 3);
+    assert_eq!(vec.capacity(), 10);
+    assert_eq!(vec.len(), 3);
 
-  vec.shrink_to(4);
-  assert_eq!(vec.capacity(), 4);
-  assert_eq!(vec.len(), 3);
+    vec.shrink_to(4);
+    assert_eq!(vec.capacity(), 4);
+    assert_eq!(vec.len(), 3);
 
-  vec.shrink_to(0);
-  assert_eq!(vec.capacity(), 3);
+    vec.shrink_to(0);
+    assert_eq!(vec.capacity(), 3);
 }
 
 #[test]
 #[should_panic]
 fn test_minivec_shrink_to_panic() {
-  let mut vec = MiniVec::<String>::with_capacity(10);
+    let mut vec = MiniVec::<String>::with_capacity(10);
 
-  vec.push(String::from("1"));
-  vec.push(String::from("2"));
-  vec.push(String::from("3"));
-  vec.push(String::from("4"));
-  vec.push(String::from("5"));
+    vec.push(String::from("1"));
+    vec.push(String::from("2"));
+    vec.push(String::from("3"));
+    vec.push(String::from("4"));
+    vec.push(String::from("5"));
 
-  vec.shrink_to(10000);
+    vec.shrink_to(10000);
 }
 
 #[test]
 fn test_minivec_extend() {
-  let mut v = mini_vec![1, 2, 3];
-  let other = mini_vec![4, 5, 6];
+    let mut v = mini_vec![1, 2, 3];
+    let other = mini_vec![4, 5, 6];
 
-  v.extend(other.iter());
+    v.extend(other.iter());
 
-  assert_eq!(v, mini_vec![1, 2, 3, 4, 5, 6]);
+    assert_eq!(v, mini_vec![1, 2, 3, 4, 5, 6]);
 
-  let mut v = mini_vec![String::from("1"), String::from("2"), String::from("3")];
-  let other = vec![String::from("4"), String::from("5"), String::from("6")];
+    let mut v = mini_vec![String::from("1"), String::from("2"), String::from("3")];
+    let other = vec![String::from("4"), String::from("5"), String::from("6")];
 
-  v.extend(other.into_iter());
+    v.extend(other.into_iter());
 
-  assert_eq!(
-    v,
-    mini_vec![
-      String::from("1"),
-      String::from("2"),
-      String::from("3"),
-      String::from("4"),
-      String::from("5"),
-      String::from("6")
-    ]
-  );
+    assert_eq!(
+        v,
+        mini_vec![
+            String::from("1"),
+            String::from("2"),
+            String::from("3"),
+            String::from("4"),
+            String::from("5"),
+            String::from("6")
+        ]
+    );
 }

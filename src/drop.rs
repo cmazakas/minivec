df84c9f5d44c867840bceac3f73be6023eb6ec87
LeonineKing1199 <christian.mazakas@gmail.com>
2021-02-22 11:13:40 -0800

    Clean up Drop impl

--- src/drop.rs
+++ src/drop.rs
@@ -4,19 +4,27 @@ use crate::MiniVec;
 
 extern crate alloc;
 
+// TODO: someday update this impl to be:
+// unsafe impl<#[may_dangle] T> for MiniVec<T>
+//
+// so that tests will pass for `test_vec_cycle`
+//
+
 impl<T> Drop for MiniVec<T> {
     fn drop(&mut self) {
         if self.buf.is_null() {
             return;
         }
 
-        #[allow(clippy::cast_ptr_alignment)]
-        let header = unsafe { core::ptr::read(self.buf as *const Header) };
+        unsafe {
+            let Header {
+                len,
+                cap,
+                alignment,
+            } = core::ptr::read(self.buf.cast::<Header>());
 
-        for i in 0..header.len {
-            unsafe { core::ptr::read(self.data().add(i)) };
-        }
-
-        unsafe { alloc::alloc::dealloc(self.buf, make_layout::<T>(header.cap, header.alignment)) };
+            core::ptr::drop_in_place(core::ptr::slice_from_raw_parts_mut(self.data(), len));
+            alloc::alloc::dealloc(self.buf, make_layout::<T>(cap, alignment));
+        };
     }
 }

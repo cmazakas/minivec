cb09a75fa78dd66235c96664dc57d755c00ffa3e
LeonineKing1199 <christian.mazakas@gmail.com>
2021-02-22 11:48:46 -0800

    Update runner script to use nightly toolchain for more clippy lints

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cargo clippy \
-  && cargo build \
+cargo +nightly clippy \
+  && cargo +nightly build \
   && CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_RUNNER="valgrind" \
      CARGO_BUILD_RUSTFLAGS="-C target-feature=+avx" \
-     cargo test $@
+     cargo +nightly test $@

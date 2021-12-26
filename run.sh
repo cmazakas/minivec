3857e53b19ee91b5319413cf05e69dd85f8558aa
Christian Mazakas <christian.mazakas@gmail.com>
2021-12-26 14:17:22 -0800

    Add valgrind suppressions file for warnings caused by `try_reserve()` tests

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 
 cargo +nightly clippy \
   && cargo +nightly build \
-  && CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_RUNNER="valgrind" \
+  && CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_RUNNER="valgrind --error-exitcode=255 --suppressions=suppressions.log" \
      CARGO_BUILD_RUSTFLAGS="-C target-feature=+avx" \
      cargo +nightly test $@ \
-  && cargo +nightly miri test $@
+  && MIRIFLAGS="-Zmiri-tag-raw-pointers -Zmiri-symbolic-alignment-check" \
+     cargo +nightly miri test $@

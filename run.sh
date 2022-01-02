7e388fc6ae367cdcd3f11aa995781787267fbea8
Christian Mazakas <christian.mazakas@gmail.com>
2022-01-02 09:44:26 -0800

    Clean up `run.sh` script

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -e
 
+set -x;
+
 cargo +nightly clippy \
   && cargo +nightly build \
-  && CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_RUNNER="valgrind --error-exitcode=255 --suppressions=suppressions.log" \
+  && CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_RUNNER="valgrind" \
      CARGO_BUILD_RUSTFLAGS="-C target-feature=+avx" \
      cargo +nightly test $@ \
   && MIRIFLAGS="-Zmiri-tag-raw-pointers -Zmiri-symbolic-alignment-check" \

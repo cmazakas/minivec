bc15ad5e1abb0bacc53f20002fa22a538a4cfa73
Christian Mazakas <christian.mazakas@gmail.com>
2022-01-22 10:20:26 -0800

    Clean up test runner scripts

--- run-benches.sh
+++ run-benches.sh
@@ -3,8 +3,5 @@
 set -x;
 
 # CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_RUNNER="valgrind --tool=callgrind" \
-RUSTFLAGS="-C target-feature=+sse2,+avx,+avx2" \
 cargo +nightly bench \
-  --features minivec_nightly \
-  --bench minivec \
-  -- --noplot
+  --features minivec_nightly
--- run-tests.sh
+++ run-tests.sh
@@ -2,16 +2,15 @@
 
 set -x;
 
-cargo +nightly clippy \
-  && cargo +nightly build \
-  && CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_RUNNER="valgrind" \
-     CARGO_BUILD_RUSTFLAGS="-C target-feature=+avx" \
-     cargo +nightly test $@ \
-  && MIRIFLAGS="-Zmiri-tag-raw-pointers -Zmiri-symbolic-alignment-check" \
-     cargo +nightly miri test $@ \
+cargo +nightly clippy --features minivec_nightly \
   && cargo +nightly build --features minivec_nightly \
   && CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_RUNNER="valgrind" \
-     CARGO_BUILD_RUSTFLAGS="-C target-feature=+avx" \
      cargo +nightly test --features minivec_nightly $@ \
   && MIRIFLAGS="-Zmiri-tag-raw-pointers -Zmiri-symbolic-alignment-check" \
      cargo +nightly miri test --features minivec_nightly $@
+#   && cargo +nightly build \
+#   && CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_RUNNER="valgrind" \
+#      CARGO_BUILD_RUSTFLAGS="-C target-feature=+avx" \
+#      cargo +nightly test $@ \
+#   && MIRIFLAGS="-Zmiri-tag-raw-pointers -Zmiri-symbolic-alignment-check" \
+#      cargo +nightly miri test $@ \
